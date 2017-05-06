d22ba3d997610f49d6b51d3e4475bce003123c50
Jeremy Folds <jmfolds@gmail.com>
2017-05-06 00:27:52 -0600

    roll in firebase to store events / auth

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import render from 'react-dom';
+import ReactFireMixin from 'reactfire';
+import reactMixin from 'react-mixin';
+const firebase = require("firebase");
 import videojs from 'video.js';
 import _ from 'lodash';
 import RadialProgress from '../RadialProgress/RadialProgress.jsx';
 
-export default class VideoPlayer extends React.Component {
+class VideoPlayer extends React.Component {
     constructor(props) {
         super(props);
         this.onVolumeChange = _.debounce(this.onVolumeChange,100);
@@ -19,73 +22,70 @@ export default class VideoPlayer extends React.Component {
             totalEndeds: 0,
             timeWatched: 0,
             totalCompletePlays: 0,
-            volume: 0
+            volume: 0,
+            // runningTotal: 0
         };
     }
+
+    onLoadedMetadata(e) {
+        let parts = this.player.tech_.el_.currentSrc.split('/');
+        let name = parts[parts.length - 1].split('.')[0];
+        this.videoId = name;
+        // grab values from database
+        firebase.database().ref(this.videoId).once('value').then((snapshot) => {
+            this.setState(snapshot.val() || {});
+            this.ogRunningTotal = this.state.runningTotal;
+        });
+    }
+
     componentDidMount() {
         // instantiate video.js
+        // other events: ['useractive']
         this.player = videojs(this.videoNode, this.props/*, this.onPlayerReady*/);
         this.setState({volume: this.player.volume()});
         this.player.on('ended', (e) => { 
             return this.onCountEvent(e);
         });
-        this.player.on('useractive', (e) => { 
-            console.log('user active');
+        this.player.on('pause', (e) => { 
+            return this.onCountEvent(e);
+        });
+        this.player.on('play', (e) => { 
+            return this.onCountEvent(e);
         });
         this.player.on('seeked', (e) => { 
             return this.onCountEvent(e);
         });
-        this.player.on('play', (e) => {
-            return this.onCountEvent(e);
+        this.player.on('loadedmetadata', (e) => { 
+            return this.onLoadedMetadata(e);
         });
         this.player.on('volumechange', (e) => { 
             return this.onVolumeChange(e);
         });
-        this.player.on('pause', (e) => { 
-            return this.onCountEvent(e)
-        });
         this.player.on('timeupdate', () => {
-                const numSegments = this.player.tech_.el_.played.length;
-                const played = [];
-                let totalPlayed = 0;
-                for (let i = 0; i < numSegments; i++) {
-                    let obj = {
-                        start: this.player.tech_.el_.played.start(i),
-                        end: this.player.tech_.el_.played.end(i)
-                    };
-                    totalPlayed += (obj.end - obj.start);
-                    played.push(obj);
-                }
-                this.setState({
-                    currentTime: this.player.currentTime(),
-                    duration: this.player.duration(),
-                    played,
-                    percentLeft: (100 - (this.state.currentTime / this.state.duration) * 100).toFixed(2),
-                    totalPlayed: (totalPlayed / this.player.duration()) * 100
-                });
-                // reset player when completely viewed
-                // and currentTime === duration
-                if (this.state.totalPlayed === 100) {
-                    if (this.state.duration === this.state.currentTime) {
-                        console.info('completely watched!');
-                        this.setState({
-                            totalCompletePlays: this.state.totalCompletePlays + 1,
-                            totalPlayed: 0
-                        });
-                        this.player.load();   
-                    }         
-                }
-            });
-    }
-
-    componentWillUnmount() {
-        if (this.player) {
-            this.player.dispose()
-        }
+            const time = this.parseTimePlayed();
+            this.setState(time);
+            const obj = JSON.parse(JSON.stringify(this.state));
+            delete obj.playedListEls;
+            firebase.database().ref(this.videoId).set(obj);
+            // firebase.database().ref(`/${this.videoId}/runningTotal`).set(this.state.runningTotal);
+            // reset player when completely viewed
+            // and currentTime === duration
+            if (this.state.totalPlayed === 100) {
+                if (this.state.duration === this.state.currentTime) {
+                    console.info('completely watched!');
+                    this.setState({
+                        totalCompletePlays: this.state.totalCompletePlays + 1,
+                        totalPlayed: 0
+                    });
+                    this.player.load();   
+                }         
+            }
+        });
     }
 
     onCountEvent(e) {
         const name = `total${_.capitalize(e.type)}s`;
+        firebase.database().ref(`/${this.videoId}/${name}`).set(this.state[name] + 1);
         this.setState({
             [name]: this.state[name] + 1
         });
@@ -95,41 +95,76 @@ export default class VideoPlayer extends React.Component {
         this.setState({
             volume: this.player.muted() ? 0: this.player.volume()
         })
+        firebase.database().ref(`/${this.videoId}/volume`).set(this.state.volume);
     }
 
     parseTimePlayed() {
-        const played = [];
+        // # viewed segments of video
+        const numSegments = this.player.tech_.el_.played.length;
+        // hold our parsed/easier to read time segments here
+        const playedLengths = [];
+        // hold our progress els for the the time segments here
+        const playedListEls = [];
+        // how much time has this video been played for
         let totalLength = 0;
-        this.state.played.forEach((p, idx) => {
+        // lifetime running time stats
+        let runningTotal = this.ogRunningTotal;
+        // parse out the time segments from the player/video
+        for (let i = 0; i < numSegments; i++) {
+            let obj = {
+                start: this.player.tech_.el_.played.start(i),
+                end: this.player.tech_.el_.played.end(i)
+            };
+            playedLengths.push(obj);
+        }
+        
+        playedLengths.forEach((p, idx) => {
             const total = this.player.duration();
-            const prev = this.state.played[idx-1];
+            const prev = playedLengths[idx-1];
             let percentMissing = 0;
             let length = idx === 0 ? p.end: p.end-p.start;
             totalLength += length;
             if (prev && prev.end !== p.start) {
                 percentMissing = ((p.start - prev.end) / total) * 100;
-                played.push(<div key={`${idx}-${idx}`} className="progress pull-left" style={{width: `${percentMissing}%`}}>
+                playedListEls.push(<div key={`${idx}-${idx}`} className="progress pull-left" style={{width: `${percentMissing}%`}}>
                     <div className="progress-bar progress-bar-opaque" role="progressbar" style={{width: "100%"}}>
                     </div>
                 </div>)
             }
             let percent = (length / total) * 100;
-            played.push(<div key={idx} className="progress pull-left" style={{width: `${percent}%`}}>
+            playedListEls.push(<div key={idx} className="progress pull-left" style={{width: `${percent}%`}}>
                 <div className="progress-bar progress-bar-fill" role="progressbar" style={{width: "100%"}}>
                 </div>
             </div>)
         });
-        return {played, totalLength};
+        runningTotal += totalLength;
+        const date = new Date(null);
+        date.setSeconds(totalLength);
+        const lengthISO = date.toISOString().substr(11, 8);
+        date.setSeconds(runningTotal);
+        const runningTotalISO = date.toISOString().substr(11, 8);
+        return {
+            currentTime: this.player.currentTime(),
+            duration: this.player.duration(),
+            percentLeft: (100 - (this.player.currentTime ()/ this.player.duration()) * 100).toFixed(2),
+            totalPlayed: (totalLength / this.player.duration()) * 100,
+            playedListEls,
+            totalLength: lengthISO,
+            runningTotal: runningTotal,
+            runningTotalISO: runningTotalISO
+        };
     }
+
+    componentWillUnmount() {
+        if (this.player) {
+            this.player.dispose()
+        }
+    }
+
     // wrap the player in a div with a `data-vjs-player` attribute so videojs won't
     // create additional wrapper in the DOM see
     // https://github.com/videojs/video.js/pull/3856
     render() {
-        const time = this.parseTimePlayed();
-        // seconds to HH:MM:SS
-        var date = new Date(null);
-        date.setSeconds(time.totalLength);
-        time.totalLength = date.toISOString().substr(11, 8);
         return (
             <div data-vjs-container>
                 <div data-vjs-player>
@@ -142,15 +177,16 @@ export default class VideoPlayer extends React.Component {
                 </div>
                 {/*video progress bars on top*/}
                 <div className="played-vis">
-                    {time.played}
+                    {this.state.playedListEls}
                 </div>
                 <div className="basic-stats">
                     Play clicks: {this.state.totalPlays} <br />
                     Pause clicks: {this.state.totalPauses} <br />
                     Times seeked: {this.state.totalSeekeds} <br />
                     Times ended: {this.state.totalEndeds} <br />
-                    Time watched: {time.totalLength} <br />
-                    Total views: {this.state.totalCompletePlays}                    
+                    Time watched: {this.state.totalLength} <br />
+                    Total views: {this.state.totalCompletePlays} <br />                  
+                    Seconds viewed overall: {this.state.runningTotalISO}             
                 </div>
                 <div className="graphs-and-charts">
                     {/* percent viewed*/}
@@ -184,3 +220,7 @@ export default class VideoPlayer extends React.Component {
         )
     }
 }
+
+reactMixin(VideoPlayer.prototype, ReactFireMixin)
+
+export default VideoPlayer;
--- src/app.js
+++ src/app.js
@@ -1,10 +1,76 @@
 import 'bootstrap-sass/assets/javascripts/bootstrap.js';
 import template from './app.html';
 require("video.js/dist/video-js.css");
+const firebase = require('firebase');
+const firebaseui = require('firebaseui');
 import React from 'react';
 import VideoPlayer from './Components/Player/Player.jsx';
 import { render } from 'react-dom';
 require("./app.scss");
+
+ var config = {
+        apiKey: "AIzaSyA9-x6IUtpo7XTyzq9Ky_Hgt5qsc9oHJMk",
+        authDomain: "vidstats-342ce.firebaseapp.com",
+        databaseURL: "https://vidstats-342ce.firebaseio.com",
+        projectId: "vidstats-342ce",
+        storageBucket: "vidstats-342ce.appspot.com",
+        messagingSenderId: "70798627814"
+    };
+    firebase.initializeApp(config);
+    // FirebaseUI config.
+      var uiConfig = {
+          callbacks: {
+            signInSuccess: () => { 
+                var app = new window.app({
+                    test: 'options'
+                });
+                return false; 
+            }
+          },
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GithubAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: 'https://vidstats.netlify.com/tos'
+      };
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+        var app = new window.app({
+            test: 'options'
+        });
+        // // User is signed in.
+        // var displayName = user.displayName;
+        // var email = user.email;
+        // var emailVerified = user.emailVerified;
+        // var photoURL = user.photoURL;
+        // var uid = user.uid;
+        // var providerData = user.providerData;
+        // user.getToken().then(function(accessToken) {
+        //     document.write('Signed in');
+        //     document.write('Sign out');
+        //     document.write(JSON.stringify({
+        //     displayName: displayName,
+        //     email: email,
+        //     emailVerified: emailVerified,
+        //     photoURL: photoURL,
+        //     uid: uid,
+        //     accessToken: accessToken,
+        //     providerData: providerData
+        //     }, null, '  '))
+        // });
+        } else {
+            // User is signed out.
+            // Initialize the FirebaseUI Widget using Firebase.
+            var ui = new firebaseui.auth.AuthUI(firebase.auth());
+            // The start method will wait until the DOM is loaded.
+            ui.start('#firebase-auth-container', uiConfig);
+        }
+    }, function(error) {
+        console.log(error);
+    });
+
+
 const videos = [
     {
         src: './data/doberman-kitten.mp4',
@@ -29,14 +95,15 @@ const videos = [
 ];
 class application {
     constructor(opts) {
+        const index = Math.floor(Math.random()*videos.length);
         const videoOpts = {
             aspectRatio: '16:5',
             autoplay: false,
             controls: true,
-            sources: [videos[Math.floor(Math.random()*videos.length)]]
+            sources: [videos[index]]
         }
         this.PlayerView = render(
-            <VideoPlayer  { ...videoOpts} />,
+            <VideoPlayer { ...videoOpts} />,
             document.getElementById('video-container')
         );
         console.info('App started with these options:', opts);
